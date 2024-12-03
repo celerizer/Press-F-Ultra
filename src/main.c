cdf0c67559a34c2c5df2ac2477ebe352df8c3823
celerizer <33245078+celerizer@users.noreply.github.com>
2024-12-02 18:48:06 -0600

    Squashed commit of the following:
    commit dbe934f4d0eda08dabf1df0bf9b22587d02521b5
    Author: celerizer <33245078+celerizer@users.noreply.github.com>
    Date:   Mon Dec 2 18:47:18 2024 -0600
    
        loading as plugin now works
    
    commit 5adb06b5c304f707cfaf9830d743ad05335e9a6b
    Author: celerizer <33245078+celerizer@users.noreply.github.com>
    Date:   Sun Dec 1 20:58:59 2024 -0600
    
        likely address mistake
    
    commit 48dba48957f9d7e462b5ac145c080c9dbc7ceb81
    Author: Robin Jones <networkfusion@users.noreply.github.com>
    Date:   Sun Dec 1 22:24:34 2024 +0000
    
        Add DevContainer environment (#6)
    
        * First attempt at dev container
    
        * Improve git ignore
    
    commit 10b730d3ddd0be109e5fc659af4abf1840bf3686
    Author: celerizer <33245078+celerizer@users.noreply.github.com>
    Date:   Sun Dec 1 16:24:07 2024 -0600
    
        Revert "Squashed commit of the following:"
    
        This reverts commit b5d12f0ea49169163f4f4f8ab25c6730005d1b2b.
    
    commit b5d12f0ea49169163f4f4f8ab25c6730005d1b2b
    Author: Keith Bourdon <33245078+celerizer@users.noreply.github.com>
    Date:   Sat Nov 30 15:49:26 2024 -0600
    
        Squashed commit of the following:
    
        commit 1b662c9e9a7a2e0a153b5d2d8b4e213173eef636
        Author: Robin Jones <networkfusion@users.noreply.github.com>
        Date:   Sat Nov 30 19:23:30 2024 +0000
    
            Improve git ignore
    
        commit d01ab74a755149d1217f11e7f3bd1139148d1ed7
        Author: Robin Jones <networkfusion@users.noreply.github.com>
        Date:   Sat Nov 30 18:56:37 2024 +0000
    
            First attempt at dev container
    
    commit b6bc16a7f0168f1ed4b40084025eb6cee844ec69
    Author: celerizer <33245078+celerizer@users.noreply.github.com>
    Date:   Sat Nov 30 12:56:53 2024 -0600
    
        cart_init before trying to run as plugin
    
    commit 1df9ac56a50dbd2ed186bf4d8840af1cc7991bbb
    Author: celerizer <33245078+celerizer@users.noreply.github.com>
    Date:   Wed Nov 27 14:48:45 2024 -0600
    
        fix offset issue
    
    commit d7e52ea5f7b6b59cc4fecb8a32e300647c315e3d
    Author: Keith Bourdon <33245078+celerizer@users.noreply.github.com>
    Date:   Wed Nov 27 12:47:26 2024 -0600
    
        max address sanity check
    
    commit 9e82e94e64e520394a9383915e0da2645c1f5a0c
    Author: Keith Bourdon <33245078+celerizer@users.noreply.github.com>
    Date:   Wed Nov 27 12:17:36 2024 -0600
    
        buildfix
    
    commit 87cb8b10a07e7f021c942a48aa1990ebd3835815
    Author: Keith Bourdon <33245078+celerizer@users.noreply.github.com>
    Date:   Wed Nov 27 10:16:34 2024 -0600
    
        try implementing flashcart plugin

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
-#include <stdint.h>
 
 #include <libdragon.h>
+#include "libcart/cart.h"
 
 #include "libpressf/src/emu.h"
 #include "libpressf/src/screen.h"
@@ -44,21 +44,79 @@ void pfu_state_set(pfu_state_type state)
   emu.state = state;
 }
 
+/**
+ * Returns the memory location of a stamped ROM when loading as a plugin, or
+ * 0 if doing so is not supported.
+ */
+static unsigned long pfu_plugin_rom_address(void)
+{
+  switch (cart_type)
+  {
+  case CART_CI:
+  case CART_SC:
+    return 0x10200000;
+  case CART_EDX:
+  case CART_ED:
+    return 0xB0200000;
+  default:
+    return 0;
+  }
+}
+
+/**
+ * Attempts to load a Channel F ROM stamped in an address by the previous
+ * program loader. Returns true if a ROM was successfully read, if the plugin
+ * feature is available.
+ * 
+ * The Channel F sanity byte $55 is checked to ensure the ROM is valid, which
+ * may exclude some older homebrew ROMs.
+ * 
+ * As the accurate ROMC mode is not used on Nintendo 64, a maximum-sized ROM
+ * can be loaded contiguously into the entire F8 address space, then
+ * overwritten later.
+ */
+static bool pfu_plugin_read_rom(void)
+{
+  u8 buffer[0xF800];
+  const unsigned long base = pfu_plugin_rom_address();
+
+  if (!base)
+    return false;
+  else
+  {
+    dma_read_async(buffer, base, sizeof(buffer));
+    dma_wait();
+
+    if (buffer[0] == 0x55)
+    {
+      f8_write(&emu.system, 0x0800, buffer, sizeof(buffer));
+      memset(buffer, 0, sizeof(buffer));
+      dma_write_raw_async(buffer, base, sizeof(buffer));
+      dma_wait();
+
+      return true;
+    }
+  }
+
+  return false;
+}
+
 int main(void)
 {
+  memset(&emu, 0, sizeof(emu));
+
   /* Initialize controller */
   joypad_init();
-
-  memset(&emu, 0, sizeof(emu));
   
   /* Initialize video */
   display_init(RESOLUTION_640x480, DEPTH_16_BPP, 2, GAMMA_NONE, FILTERS_RESAMPLE);
   rdpq_init();
-  emu.video_buffer = (uint16_t*)malloc_uncached_aligned(64, SCREEN_WIDTH * SCREEN_HEIGHT * 2);
+  emu.video_buffer = (u16*)malloc_uncached_aligned(64, SCREEN_WIDTH * SCREEN_HEIGHT * 2);
   emu.video_frame = surface_make_linear(emu.video_buffer, FMT_RGBA16, SCREEN_WIDTH, SCREEN_HEIGHT);
   emu.video_scaling = PFU_SCALING_4_3;
 
   /* Initialize assets */
+  cart_init();
   dfs_init(DFS_DEFAULT_LOCATION);
 
   /* Initialize fonts */
@@ -83,7 +141,12 @@ int main(void)
   pressf_init(&emu.system);
   f8_system_init(&emu.system, &pf_systems[0]);
   pfu_menu_init();
-  pfu_menu_switch_roms();
+
+  /* If loaded as plugin, jump to loaded ROM, otherwise load ROM menu */
+  if (pfu_plugin_read_rom())
+    pfu_state_set(PFU_STATE_EMU);
+  else
+    pfu_menu_switch_roms();
 
   while (64)
   {
